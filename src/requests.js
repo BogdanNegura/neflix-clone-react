2e791881cbeee8ffb6bd011da6e7134b32f776e6
Bogdan Negura <id.bogdann@yahoo.com>
2020-09-08 22:25:38 +0100

    initial setup and create first component

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "c08be77a24c6e0370959fac7d32c9fce";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=eu-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=eu-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
