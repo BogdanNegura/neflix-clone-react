408f26bb9203601e86bf8623bb5cd9b77b281ac9
Bogdan Negura <id.bogdann@yahoo.com>
2020-09-09 21:48:20 +0100

    fetch data using async request with axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
 import Row from "./components/row/row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
       <h1>NetFlix Clone</h1>
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="TRENDING NOW" />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
