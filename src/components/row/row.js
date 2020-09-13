8f13fbb58ca85e0bb1aed63d8351b9505bf4e68c
Bogdan Negura <id.bogdann@yahoo.com>
2020-09-13 22:03:44 +0100

    pull title and images from API and add some style to it

--- src/components/row/row.js
+++ src/components/row/row.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios from "../../axios";
+
+import "./row.css";
+
+const baseUrl = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
@@ -9,14 +13,26 @@ function Row({ title, fetchUrl }) {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
       console.log(request.data.results);
+      setMovies(request.data.results);
       return request;
     }
     fetchData();
-  }, []);
+  }, [fetchUrl]);
 
+  console.log(movies);
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
+      <div className="row_posters">
+        {/* Several row_poster(s) */}{" "}
+        {movies.map((movie) => (
+          <img
+            className="row_poster"
+            src={`${baseUrl}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
