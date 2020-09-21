2ef058f4a92524e050307c6fd2be8cf4144d9d62
Bogdan Negura <id.bogdann@yahoo.com>
2020-09-21 21:00:33 +0100

    animate nav bar on scroll

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./nav.css";
 
 function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav_black"}`}>
       <img
         className="nav__logo"
-        src="https://en.wikipedia.org/wiki/Netflix#/media/File:Netflix_logo.svg"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
         alt="Netflix Logo"
       />
       <img
         className="nav__avatar"
-        src="https://pbs.twimg.com/profile/124011999041155"
+        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
         alt="Netflix Logo"
       />
     </div>
